5abdcec04e63bbd6364a73947f231d44415659ae
Twall0 <twall0010@gmail.com>
2025-06-03 19:09:40 -0400

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20250603_190941.cpp
@@ -0,0 +1,46 @@
+// Last updated: 6/3/2025, 7:09:41 PM
+// Hash maps are used to reduce the time complexity to O(N)
+class Solution {
+public:
+/*
+    ====================================
+    ============  O(N) ===============
+    ====================================
+    */
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (um.find(complement) != um.end()) {
+                return {um[complement], i};
+            }
+            um[nums[i]] = i;
+        }
+
+        return {};
+    }
+
+
+    /*
+    ====================================
+    ============  O(N^2) ===============
+    ====================================
+
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+    vector<int> vec;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    vec = {i,j};
+                    break;
+                }
+            }
+       }
+       return vec;
+    }
+    */
+
+    
+};
